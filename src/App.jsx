8eb33ee91414a863f052207deaa8ae2c9dc6fd92
Azimboy <azimboy383@gmail.com>
2023-07-25 10:20:33 +0200

    remolo-pizza

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import { Layout, Cart } from "./components";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { routes, adminRoutes } from "./utils/routes";
+import useIsAuth from "./hooks/uselsAuth";
+import { CartMobile } from "./components/cart/CartMobile";
+
+function App() {
+  const {pathname } = useLocation();
+  const {isAuth: auth} = useSelector((state) => state.auth)
+  const isCart = ['/', '/favourite']
+  const isAuth = useIsAuth()
+
+  return (
+    <>
+      <Layout />
+      <div id="main-content__row">
+        <Routes>
+          {
+          routes.map(({id, path, component}) => (
+          <Route key={id} path={path} element={component} />
+          ))}
+            {auth ? adminRoutes.map(({id, path, component}) => (
+              <Route key={id} path={path} element={component} />
+              )) 
+            : null} 
+        </Routes> 
+        {isCart.includes(pathname) ? <Cart /> : null}  
+      </div>
+        <CartMobile/>
+        
+    </>
+  );
+}
+
+export default App;
